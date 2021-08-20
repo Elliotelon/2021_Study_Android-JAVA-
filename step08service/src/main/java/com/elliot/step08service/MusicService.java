824122e43d78dfa0f56668abafb854becbc25df2
kimminkyu <kmk50009@naver.com>
2021-08-21 00:16:11 +0900

    StudyJava Step08Service

---
+++ step08service/src/main/java/com/elliot/step08service/MusicService.java
@@ -0,0 +1,42 @@
+package com.elliot.step08service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class MusicService extends Service {
+
+    MediaPlayer mediaPlayer;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mediaPlayer = MediaPlayer.create(this, R.raw.mp3piano);
+        mediaPlayer.setLooping(false);
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        mediaPlayer.start();
+
+        return super.onStartCommand(intent, flags, startId);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.stop();
+    }
+}
